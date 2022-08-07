3ea971172e0627f8deff4b526d82dd2cef9bad58
FAMASoon <famasoon@gmail.com>
2022-08-07 14:53:48 +0900

    lint with clippy

--- ch1/sha1_cracker/src/main.rs
+++ ch1/sha1_cracker/src/main.rs
@@ -29,7 +29,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         let line = line?;
         let common_password = line.trim();
 
-        if hash_to_crack == &hex::encode(sha1::Sha1::digest(common_password.as_bytes())) {
+        if hash_to_crack == hex::encode(sha1::Sha1::digest(common_password.as_bytes())) {
             println!("Password found {}", &common_password);
             return Ok(());
         }
