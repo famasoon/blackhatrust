763b3b2e5bfb78cbe30ea813b237d0acedc91c7e
FAMASoon <famasoon@gmail.com>
2022-08-07 16:58:42 +0900

    Change for more speed

--- ch2/tricoder/src/ports.rs
+++ ch2/tricoder/src/ports.rs
@@ -2,10 +2,9 @@ use crate::{
     common_ports::MOST_COMMON_PORTS_100,
     model::{Port, Subdomain},
 };
-
+use rayon::prelude::*;
 use std::net::{SocketAddr, ToSocketAddrs};
 use std::{net::TcpStream, time::Duration};
-use rayon::prelude::*;
 
 pub fn scan_ports(mut subdomain: Subdomain) -> Subdomain {
     let socket_addresses: Vec<SocketAddr> = format!("{}:1024", subdomain.domain)
@@ -13,14 +12,14 @@ pub fn scan_ports(mut subdomain: Subdomain) -> Subdomain {
         .expect("port scanner: Creating socket address")
         .collect();
 
-    if socket_addresses.len() == 0 {
+    if socket_addresses.is_empty() {
         return subdomain;
     }
 
     subdomain.open_ports = MOST_COMMON_PORTS_100
-        .into_iter()
+        .into_par_iter()
         .map(|port| scan_port(socket_addresses[0], *port))
-        .filter(|port| port.is_open)
+        .filter(|port| port.is_open) // filter closed ports
         .collect();
     subdomain
 }
@@ -29,14 +28,7 @@ fn scan_port(mut socket_address: SocketAddr, port: u16) -> Port {
     let timeout = Duration::from_secs(3);
     socket_address.set_port(port);
 
-    let is_open = if let Ok(_) = TcpStream::connect_timeout(&socket_address, timeout) {
-        true
-    } else {
-        false
-    };
+    let is_open = TcpStream::connect_timeout(&socket_address, timeout).is_ok();
 
-    Port {
-        port: port,
-        is_open,
-    }
+    Port { port, is_open }
 }
